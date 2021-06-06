c6e5e7a5e063873f0d5ca9362f4b755237406353
123567-wq <85429413+123567-wq@users.noreply.github.com>
2021-06-05 23:55:28 -0700

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header ( ){
+  return(
+   <header>
+<h1> BOOTCAMPREVIEW </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Note ( ){
+  return(
+   <div>
+     <h1> javascript and React.js </h1>
+    <p> This was an excellent Bootcamp taken by shaurya sinha. I did not know about javascript using React.js before the Bootcamp and really much helpful for me. please do more.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
